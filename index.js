f60cf5ca1f0a5d2c88cfe6962f274e76d73a2a7a
Your Name <you@example.com>
2020-05-10 09:10:24 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookiesession = require('cookie-session');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+//const authRoutes = require('./routes/authRoutes');
+//authRoutes(app);
+//for the above code..it is shorthand as below:
+app.use(
+    cookiesession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
